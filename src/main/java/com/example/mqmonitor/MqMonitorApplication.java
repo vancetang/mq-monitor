6e4821fae6534d6629efa68ad0ef521db389077c
Vance <vance.tw@gmail.com>
2025-11-11 20:52:46 +0800

    refactor(app): migrate MQMonitorApplication to Liberty deployment

--- src/main/java/com/example/mqmonitor/MqMonitorApplication.java
+++ src/main/java/com/example/mqmonitor/MqMonitorApplication.java
@@ -2,6 +2,8 @@ package com.example.mqmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,11 +11,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @EnableScheduling
 @SpringBootApplication
-public class MqMonitorApplication {
+public class MqMonitorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MqMonitorApplication.class, args);
 		log.info("MQ Monitor Application started");
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MqMonitorApplication.class);
+	}
 }
